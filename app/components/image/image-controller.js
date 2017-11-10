86f5a53fe907804e5ad5e0b9baa5e6dfa489e116
Sean MacKenzie <sean.andrew.mackenzie@gmail.com>
2017-11-10 10:46:18 -0700

    Controllers bound to init, first push up

--- app/components/image/image-controller.js
+++ app/components/image/image-controller.js
@@ -1,6 +1,10 @@
 function ImageController() {
 	//Your ImageService is a global constructor function what can you do here if you new it up?
+	var imageService = new ImageService()
 
+	imageService.getImage(function(image) {
+		console.log(image);
+	})
 }
 
 
--- app/init.js
+++ app/init.js
@@ -2,5 +2,9 @@
 var app = {
   controllers:{
     weatherController: new WeatherController(),
+    imageController: new ImageController(),
+    todoController: new TodoController(),
+    quoteController: new QuoteController()
+    
   }
 }
